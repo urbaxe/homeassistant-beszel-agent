d2c51e3098b922d3cb62e8ad46fa54a1f8defafe
matthewhadley <matthew.hadley@gmail.com>
2024-12-18 22:14:31 -0600

    fix: pass key value

--- beszel-agent/run.sh
+++ beszel-agent/run.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
-DROPBACK_VERSION=$(cat VERSION)
+VERSION=$(cat VERSION)
 PUBLIC_KEY="$(bashio::config 'public_key')"
 
 # add date to default bashio log timestamp
 declare __BASHIO_LOG_TIMESTAMP="%Y-%m-%d %T"
 
-bashio::log.info "Beszel-Agent $DROPBACK_VERSION"
+bashio::log.info "Beszel-Agent $VERSION"
 
-pwd;
-ls -al;
-
-PORT=45876 PUBLIC_KEY="$PUBLIC_KEY" ./beszel-agent
+PORT=45876 KEY="$PUBLIC_KEY" ./beszel-agent
