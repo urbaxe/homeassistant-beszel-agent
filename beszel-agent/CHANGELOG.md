6f55e2b758a02d7559075d78947fd54950096e47
matthewhadley <matthew.hadley@gmail.com>
2025-01-06 15:08:30 -0600

    added: v0.9.1 beszel_agent

--- beszel-agent/CHANGELOG.md
+++ beszel-agent/CHANGELOG.md
@@ -1,3 +1,10 @@
+## [0.9.0] - 2025-01-06
+
+### Added
+
+- Beszel-Agent 0.9.1
+- Output Beszel-Agent version at startup
+
 ## [0.8.0] - 2024-12-19
 
 ### Changed
--- beszel-agent/run.sh
+++ beszel-agent/run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
 VERSION=$(cat VERSION)
+BESZEL_AGENT_VERSION=$(cat BESZEL_AGENT_VERSION)
 PUBLIC_KEY="$(bashio::config 'public_key')"
 
 # add date to default bashio log timestamp
 declare __BASHIO_LOG_TIMESTAMP="%Y-%m-%d %T"
 
-bashio::log.info "Beszel-Agent Home Assistant Add-On $VERSION "
+bashio::log.info "Beszel-Agent Home Assistant Add-On $VERSION"
+bashio::log.info "Beszel-Agent $BESZEL_AGENT_VERSION"
 
 PORT=45876 KEY="$PUBLIC_KEY" ./beszel-agent
